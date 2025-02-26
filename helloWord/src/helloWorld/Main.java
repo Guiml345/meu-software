dd95db0787f895a4d4e46503bcec36e97f862651
Guiml345 <guiml3456543@gmail.com>
2025-02-26 20:38:39 -0300

    Novo Scaner

--- helloWord/src/helloWorld/Main.java
+++ helloWord/src/helloWorld/Main.java
@@ -1,10 +1,15 @@
 package helloWorld;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World !");
+		Scanner scan = new Scanner( System.in);
+		System.out.println("Digite um nome: ");
+		String nome = scan.next();
+		System.out.println("Hello " + nome +", World !");
 	}
 
 }
